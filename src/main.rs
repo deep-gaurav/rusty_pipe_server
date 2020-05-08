eb52aaa74e75e2af9ca822a0838f6823a73785dc
Deep Gaurav <deepgauravraj@gmail.com>
2020-05-09 04:03:40 +0530

    Add many metadata

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-//!
-//! This example demonstrates async/await usage with warp.
-//! NOTE: this uses tokio 0.1 , not the alpha tokio 0.2.
+use serde::{Serialize,Deserialize};
 
 use async_trait::async_trait;
 use rusty_pipe::downloader_trait::Downloader;
@@ -8,6 +6,10 @@ use rusty_pipe::youtube_extractor::stream_extractor::YTStreamExtractor;
 
 use juniper::{EmptyMutation, EmptySubscription, FieldError, RootNode};
 use warp::{http::Response, Filter};
+use std::collections::hash_map::RandomState;
+use std::collections::HashMap;
+use std::str::FromStr;
+
 // use juniper_warp::
 struct DownloaderObj;
 
@@ -21,6 +23,19 @@ impl Downloader for DownloaderObj {
         // println!("suceess query");
         Ok(String::from(body))
     }
+
+    async fn download_with_header(&self, url: &str, header: HashMap<String, String, RandomState>) -> Result<String, String> {
+        let client=reqwest::Client::new();
+        let res = client.get(url);
+        let mut headers = reqwest::header::HeaderMap::new();
+        for header in header{
+            headers.insert(reqwest::header::HeaderName::from_str(&header.0).map_err(|e|e.to_string())?, header.1.parse().unwrap());
+        }
+        let res = res.headers(headers);
+        let res = res.send().await.map_err(|er|er.to_string())?;
+        let body = res.text().await.map_err(|er|er.to_string())?;
+        Ok(String::from(body))
+    }
 }
 
 #[derive(Clone)]
@@ -32,45 +47,128 @@ struct YoutubeStreamItem {
 }
 #[juniper::graphql_object(Context = Context)]
 impl YoutubeStreamItem {
-    async fn video_streams(&self) -> Result<Vec<StreamItem>, FieldError> {
-        let streams = self.extractor.get_video_stream().await?;
+    fn video_streams(&self) -> Result<Vec<StreamItem>, FieldError> {
+        let streams = self.extractor.get_video_stream()?;
         let mut v = vec![];
         for stream in streams {
-            v.push(StreamItem {
-                url: stream.url,
-                resolution: stream.itag.resolution_string,
-            });
+            let stream_str = serde_json::to_string(&stream)?;
+            v.push(
+                serde_json::from_str(&stream_str)?
+            );
         }
         Ok(v)
     }
-    async fn video_only_streams(&self) -> Result<Vec<StreamItem>, FieldError> {
-        let streams = self.extractor.get_video_only_stream().await?;
+    fn video_only_streams(&self) -> Result<Vec<StreamItem>, FieldError> {
+        let streams = self.extractor.get_video_only_stream()?;
         let mut v = vec![];
         for stream in streams {
-            v.push(StreamItem {
-                url: stream.url,
-                resolution: stream.itag.resolution_string,
-            });
+            let stream_str = serde_json::to_string(&stream)?;
+            v.push(
+                serde_json::from_str(&stream_str)?
+            );
         }
         Ok(v)
     }
-    async fn audio_only_streams(&self) -> Result<Vec<StreamItem>, FieldError> {
-        let streams = self.extractor.get_audio_streams().await?;
+    fn audio_only_streams(&self) -> Result<Vec<StreamItem>, FieldError> {
+        let streams = self.extractor.get_audio_streams()?;
         let mut v = vec![];
         for stream in streams {
-            v.push(StreamItem {
-                url: stream.url,
-                resolution: stream.itag.resolution_string,
-            });
+            let stream_str = serde_json::to_string(&stream)?;
+            v.push(
+                serde_json::from_str(&stream_str)?
+            );
         }
         Ok(v)
     }
+
+    fn title(&self)->Result<String,FieldError>{
+        Ok(self.extractor.get_name()?)
+    }
+
+    fn description(&self)->Result<String,FieldError>{
+        Ok(self.extractor.get_description(false)?.0)
+    }
+
+    fn uploader_name(&self)->Result<String,FieldError>{
+        Ok(self.extractor.get_uploader_name()?)
+    }
+
+    fn uploader_url(&self)->Result<String,FieldError>{
+        Ok(self.extractor.get_uploader_url()?)
+    }
+
+    fn video_thumbnails(&self)->Result<Vec<Thumbnail>,FieldError>{
+        let thumbs = self.extractor.get_video_thumbnails()?;
+        let mut thumbf = vec![];
+        for thumb in thumbs{
+            thumbf.push(
+                Thumbnail{
+                    url:thumb.url,
+                    height: thumb.height as i32,
+                    width: thumb.width as i32
+                }
+            )
+        }
+        Ok(thumbf)
+    }
+
+    fn uploader_thumbnails(&self)->Result<Vec<Thumbnail>,FieldError>{
+        let thumbs = self.extractor.get_uploader_avatar_url()?;
+        let mut thumbf = vec![];
+        for thumb in thumbs{
+            thumbf.push(
+                Thumbnail{
+                    url:thumb.url,
+                    height: thumb.height as i32,
+                    width: thumb.width as i32
+                }
+            )
+        }
+        Ok(thumbf)
+    }
+
+    fn likes(&self)->Result<i32,FieldError>{
+        Ok(self.extractor.get_like_count()? as i32)
+    }
+
+    fn dislikes(&self)->Result<i32,FieldError>{
+        Ok(self.extractor.get_dislike_count()? as i32)
+    }
+
+    fn views(&self)->Result<i32,FieldError>{
+        Ok(self.extractor.get_view_count()? as i32)
+    }
+
+    fn length(&self)->Result<i32,FieldError>{
+        Ok(self.extractor.get_length()? as i32)
+    }
+
 }
 
-#[derive(juniper::GraphQLObject)]
-struct StreamItem {
-    url: String,
-    resolution: String,
+#[derive(juniper::GraphQLObject,Serialize,Deserialize)]
+pub struct StreamItem {
+    pub url: String,
+    pub itag: i32,
+    pub approxDurationMs: String,
+    pub audioChannels: Option<i32>,
+    pub audioQuality: Option<String>,
+    pub audioSampleRate: Option<String>,
+    pub averageBitrate: Option<i32>,
+    pub bitrate: i32,
+    pub contentLength: Option<String>,
+    pub height: Option<i32>,
+    pub width: Option<i32>,
+    pub quality: String,
+    pub qualityLabel: Option<String>,
+    pub lastModified: String,
+    pub mimeType: String,
+}
+
+#[derive(juniper::GraphQLObject,Serialize,Deserialize)]
+pub struct Thumbnail{
+    url:String,
+    width:i32,
+    height:i32
 }
 
 struct Query;
